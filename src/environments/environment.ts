6385bf576d6f45768d20a61e1a61bbcb16678840
DAS UPN <ginoaalexander@gmail.com>
2025-10-29 21:11:23 -0500

    Configuracion de la API url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false, // Indica que es un entorno de desarrollo (si es producción, cambiar a true)
-  apiUrl: 'http://localhost:8012/backend/api' // Aquí debes poner la URL de tu API PHP (ajusta el puerto si es necesario.
+  apiUrl: 'http://localhost:8012/backend/api' // Aquí debes poner la URL de tu API PHP (ajusta el puerto si es necesario).
 };
